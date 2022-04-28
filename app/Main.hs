c8c6a53a0eb5bd01e79269a8eaf501a70104c216
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-04-28 11:36:53 +0300

    .

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Lib ( perms, magic3, magic4)
-import Data.Time.Clock
+import Data.Time.Clock ( diffUTCTime, getCurrentTime )
 
---main :: IO ()
+
 main = do
    t0 <- getCurrentTime 
    print $ "start -- " ++ show t0 
-   print $ length magic4
+   print $ length magic3
    t1 <- getCurrentTime 
    print $ diffUTCTime t1 t0
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,44 +6,59 @@ module Lib ( perms, magic3, magic4) where
 import Data.List ( (\\) )
 import Control.Monad (when)
 import Text.ParserCombinators.ReadP (many1)
+import Debug.Trace
 
+perms :: [Int] -> Int -> [[Int]]
 perms xs 1 = map (:[]) xs
-perms xs n = do 
+perms xs n = do
    x <- xs
    let xs' = xs \\ [x]
    rest <- perms xs' (n-1)
    return (x : rest)
 
-magic3 = let 
+magic3 = let
    n = 3
-   xs = [1..n^2]
+   ns = [1..n^2]
    nSum = (n^2 + 1) * n `div` 2
+   level ns = do 
+      ys <- filter (\p -> nSum == sum p) (perms ns n)
+      let ns' = ns \\ ys
+      [(ys, ns')]
 
-   stack = do
-      ys1 <- filter (\p -> nSum == sum p) (perms xs n)
-      let ns2 = xs \\ ys1
-      ys2 <- filter (\p -> nSum == sum p) (perms ns2 n)
-      let ns3 = ns2 \\ ys2
-      ys3 <- filter (\p -> nSum == sum p) (perms ns3 n)
-      return [ys1, ys2, ys3]
+   stack ns = 
+      if ns /= [] 
+      then do 
+        (ys, ns') <- level ns
+        ss <- stack ns'
+        return $ ys : ss 
+      else [[]]
+        
 
-   ok m = sum (map (!!0) m) == nSum && 
-          sum (map (!!1) m) == nSum && 
+
+      -- ys1 <- filter (\p -> nSum == sum p) (perms ns1 n)
+      -- let ns2 = ns1 \\ ys1
+      -- ys2 <- filter (\p -> nSum == sum p) (perms ns2 n)
+      -- let ns3 = ns2 \\ ys2
+      -- ys3 <- filter (\p -> nSum == sum p) (perms ns3 n)
+      -- return [ys1, ys2, ys3]
+
+   ok m = sum (map (!!0) m) == nSum &&
+          sum (map (!!1) m) == nSum &&
           sum (map (!!2) m) == nSum &&
-          m!!0!!0 + m!!1!!1 + m!!2!!2 == nSum && 
-          m!!0!!2 + m!!1!!1 + m!!2!!0 == nSum                
- in 
-    filter ok stack
+          m!!0!!0 + m!!1!!1 + m!!2!!2 == nSum &&
+          m!!0!!2 + m!!1!!1 + m!!2!!0 == nSum
+ in
+    filter ok (stack ns)
 
 
-magic4 = let 
+magic4 = let
    n = 4
    ns1 = [1..n^2]
    nSum = (n^2 + 1) * n `div` 2
 
    stack = do
       ys1 <- filter (\p -> nSum == sum p) (perms ns1 n)
-      let ns2 = ns1 \\ ys1
+      let ns2 = ns1 \\ ys1 -- (trace ("ys1=" ++ show ys1) ys1)
       ys2 <- filter (\p -> nSum == sum p) (perms ns2 n)
       let ns3 = ns2 \\ ys2
       ys3 <- filter (\p -> nSum == sum p) (perms ns3 n)
@@ -52,13 +67,13 @@ magic4 = let
 
       return [ys1, ys2, ys3, ys4]
 
-   ok m = sum (map (!!0) m) == nSum && 
-          sum (map (!!1) m) == nSum && 
+   ok m = sum (map (!!0) m) == nSum &&
+          sum (map (!!1) m) == nSum &&
           sum (map (!!2) m) == nSum &&
           sum (map (!!3) m) == nSum &&
-          m!!0!!0 + m!!1!!1 + m!!2!!2 + m!!3!!3 == nSum && 
-          m!!0!!3 + m!!1!!2 + m!!2!!1 + m!!3!!0 == nSum                
- in 
+          m!!0!!0 + m!!1!!1 + m!!2!!2 + m!!3!!3 == nSum &&
+          m!!0!!3 + m!!1!!2 + m!!2!!1 + m!!3!!0 == nSum
+ in
     filter ok stack
 
 
