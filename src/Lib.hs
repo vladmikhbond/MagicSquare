16f1622a81dee426dc69fcad5c7184232939e83f
volodymyr bondariev <vladmikhbond@hotmail.com>
2022-04-29 20:41:49 +0300

    .

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,12 +6,14 @@
 -- ("g","vcm")
 -- ("gm", "vc")
 -- ("", "vgcm")
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
-import Data.List ( (\\), sort)
+import Data.List ( (\\), sort, isPrefixOf)
+
 
 -- состояние - упоряд. множ. объектов, включая лодку, на левом берегу (множ на правом = разность)
-type State = [Char]  
-type Hist  = [State] 
+type State = [Char]
+type Hist  = [State]
 
 boat : goods = "_cgv"
 invalidStates = map sort ["gv", "cg", "cgv", "_c", "_v", "_"]
@@ -25,18 +27,36 @@ nextStates state = let
    states = if boat `elem` state
     then thisBank : [ thisBank \\ [x] | x <- thisBank]
     else (boat : state) : [boat : x : state | x <- otherBank]
- in 
+ in
    map sort states
 
 isValid :: State -> Bool
 isValid state = state `notElem` invalidStates
 
 variants :: Hist -> [Hist]
-variants hist = do 
-   let nexts = filter isValid (nextStates $ head hist) 
-   next <- filter (`notElem` hist) nexts 
-   if null next 
-     then return $ reverse hist   
+variants hist = do
+   let nexts = filter isValid (nextStates $ head hist)
+   next <- filter (`notElem` hist) nexts
+   if null next
+     then return $ reverse ("" : hist)
      else variants (next : hist)
-      
-solve = variants [boat : "cgv"]
+
+solve x = variants [sort x]
+
+-- shipping ["_cgv","cv","_cv","c","_cg","g","_g",""]
+shipping :: Hist -> Hist
+shipping hist = tail $ zipWith g hist ("" : hist)
+ where
+    g x y = if length x <= length y
+       then '>' : (y \\ x)
+       else '<' : (x \\ y)
+
+m x = do
+   let h = (shipping . head . solve) x
+   mapM_ showBoat h
+
+showBoat :: String -> IO()
+showBoat (c : _ : cs) = putStrLn  $ "\\_"++ g ++"_/ " ++ [c]
+    where g = if null cs then "_" else cs 
+
+
